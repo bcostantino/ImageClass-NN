85035de2fca1b82baefdac806f1771dd2aa625e1
bcostantino <costan96@students.rowan.edu>
2021-12-26 13:53:50 -0500

    clean up

--- main.m
+++ main.m
@@ -1,18 +1,13 @@
 % main.m
 %  main MATLAB script to handle training the NN
+clear;
 
 fname='/networkData';
 if isfile(fname)
     load('./networkData.mat','XTrain','YTrain','XVal','YVal','XTest','YTest');
 else
     trainPerc=0.6;
-    valPerc=0.0;
+    valPerc=0.2;
     [XTrain,YTrain,XVal,YVal,XTest,YTest]=prepNetworkData(trainPerc,valPerc);
 end
 
-for i=1:1000
-    imshow(XTrain(:,:,:,i));
-    truesize([200 200])
-    title(YTrain(i));
-    pause(5);
-end
